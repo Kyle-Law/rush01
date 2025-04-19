c3cb6649077c4a3a012c1934b8745d4e1639097d
Zhun Khing Law <zlaw@c1r4s9.42iskandarputeri.edu.my>
2025-04-19 17:51:25 +0800

    Fix view2.c

--- ex00/view2.c
+++ ex00/view2.c
@@ -6,14 +6,14 @@
 /*   By: hbinti-d <hbinti-d@student.42iskandar      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/19 17:07:53 by hbinti-d          #+#    #+#             */
-/*   Updated: 2025/04/19 17:28:26 by hbinti-d         ###   ########.fr       */
+/*   Updated: 2025/04/19 17:51:18 by zlaw             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int     is_in_row(char **puzzle_board, int row, int column, int digit)
+int	is_in_row(char **puzzle_board, int row, int column, int digit)
 {
-	int     i;
-	
+	int	i;
+
 	i = 1;
 	while (i < column)
 	{
@@ -24,19 +24,16 @@ int     is_in_row(char **puzzle_board, int row, int column, int digit)
 	return (0);
 }
 
-
 int	is_in_column(char **puzzle_board, int row, int column, int digit)
 {
 	int	i;
 
 	i = 1;
 	while (i < row)
-    {
-        // If the digit received is found in the column, the return is 1
+	{
 		if (puzzle_board[i][column] == digit + '0')
 			return (1);
-        i++;
-    }
-    // If it doesn't find the value, the return is 0
+		i++;
+	}
 	return (0);
 }
