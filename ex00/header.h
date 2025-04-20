2452fa17a59262eb425ffcfcb21166a64a360bf2
Zhun Khing Law <zlaw@c1r3s6.42iskandarputeri.edu.my>
2025-04-20 17:16:13 +0800

    Fixx

--- ex00/header.h
+++ ex00/header.h
@@ -6,7 +6,7 @@
 /*   By: hbinti-d <hbinti-d@student.42iskandar      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/19 10:55:05 by hbinti-d          #+#    #+#             */
-/*   Updated: 2025/04/20 16:49:35 by zlaw             ###   ########.fr       */
+/*   Updated: 2025/04/20 17:14:51 by zlaw             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,5 +39,6 @@ int		is_in_row(char **puzzle_board, int row, int column, int digit);
 int		is_correct_row(char **board, int row);
 int		is_correct_col(char **board, int col);
 void	rush(char **clues);
-void	mark_success(char **puzzle_board);
+int		mark_success(char **puzzle_board);
+void	solve_recursive(char **board, int row, int col);
 #endif
--- ex00/print.c
+++ ex00/print.c
@@ -6,7 +6,7 @@
 /*   By: surajend <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/19 14:07:26 by surajend          #+#    #+#             */
-/*   Updated: 2025/04/20 16:49:13 by zlaw             ###   ########.fr       */
+/*   Updated: 2025/04/20 17:06:41 by zlaw             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,11 +22,10 @@ void	print_error(void)
 	write(1, "Error\n", 6);
 }
 
-void	mark_success(char **puzzle_board)
+int	mark_success(char **puzzle_board)
 {
-	if (puzzle_board[0][0] == '1')
-		return ;
 	puzzle_board[0][0] = '1';
+	return (1);
 }
 
 void	print_solution(char **puzzle_board)
@@ -35,9 +34,6 @@ void	print_solution(char **puzzle_board)
 	int	j;
 
 	i = 1;
-	if (puzzle_board[0][0] == '1')
-		return ;
-	puzzle_board[0][0] = '1';
 	while (i <= 4)
 	{
 		j = 1;
